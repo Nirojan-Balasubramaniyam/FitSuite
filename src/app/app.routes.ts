e7cb1c321d9d529564dbdaa7d3390df19503a78a
thushag <iamjenittahs@gmail.com>
2024-12-10 13:38:57 +0530

    Routing paths created for landing and member

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,8 +11,41 @@ import { PaymentComponent } from './Views/Admin/payment/payment.component';
 import { ProgramEnrollComponent } from './Views/Admin/program-enroll/program-enroll.component';
 import { ProgramManagementComponent } from './Views/Admin/program-management/program-management.component';
 import { ProgramReportComponent } from './Views/Admin/program-report/program-report.component';
+import { LandingLayoutComponent } from './Layouts/landing-layout/landing-layout.component';
+import { RegisterComponent } from './Views/Landing/register/register.component';
+import { LoginComponent } from './Views/Landing/login/login.component';
+import { AboutUsComponent } from './Views/Landing/about-us/about-us.component';
+import { BMIComponent } from './Views/Landing/bmi/bmi.component';
+import { ContactUsComponent } from './Views/Landing/contact-us/contact-us.component';
+import { HomeComponent } from './Views/Landing/home/home.component';
+import { ProgrammesComponent } from './Views/Landing/programmes/programmes.component';
+import { PackagesComponent } from './Views/Landing/packages/packages.component';
+import { MemberLayoutComponent } from './Layouts/member-layout/member-layout.component';
+import { ChangeInfoComponent } from './Views/Member/change-info/change-info.component';
+import { ChangeProgramComponent } from './Views/Member/change-program/change-program.component';
+import { PaymentHistoryComponent } from './Views/Member/payment-history/payment-history.component';
+import { ProfileComponent } from './Views/Member/profile/profile.component';
+import { TrainingPackageComponent } from './Views/Member/training-package/training-package.component';
+import { UpdateSkillsComponent } from './Views/Member/update-skills/update-skills.component';
+import { MemberBmiComponent } from './Views/Member/member-bmi/member-bmi.component';
 
 export const routes: Routes = [
+
+    {
+        path: '',
+        component: LandingLayoutComponent,
+        children: [
+            { path: 'registration', component: RegisterComponent},
+            { path: 'login', component: LoginComponent },
+            { path: 'about', component: AboutUsComponent },
+            { path: 'bmi', component: BMIComponent },
+            { path: 'contact', component: ContactUsComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'programs', component: ProgrammesComponent },
+            { path: 'package', component: PackagesComponent },
+        ]
+    },
+
     {
         path: 'admin',
         component: AdminLayoutComponent,
@@ -28,6 +61,21 @@ export const routes: Routes = [
             { path: 'programs', component: ProgramManagementComponent },
             { path: 'program-report', component: ProgramReportComponent },
             { path: 'approval-request',component:ApprovalRequestComponent}
+        ]
+    },
+
+    {
+        path: 'member',
+        component: MemberLayoutComponent,
+        children: [
+            { path: 'bmi', component: MemberBmiComponent },
+            { path: 'change-info', component: ChangeInfoComponent },
+            { path: 'change-program', component: ChangeProgramComponent },
+            { path: 'payment', component: PaymentComponent },
+            { path: 'payment-history', component: PaymentHistoryComponent },
+            { path: 'profile', component: ProfileComponent },
+            { path: 'training-package', component: TrainingPackageComponent },
+            { path: 'skills', component: UpdateSkillsComponent }
         ]
     }
 ];
