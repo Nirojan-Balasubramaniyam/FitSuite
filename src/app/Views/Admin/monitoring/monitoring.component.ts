b47498dd44c911cd5d66713bdf0c597858ab8d69
anojan <anojan@unicornconnectedapps.com>
2025-02-02 22:11:55 +0530

    first commit

--- src/app/Views/Admin/monitoring/monitoring.component.ts
+++ src/app/Views/Admin/monitoring/monitoring.component.ts
@@ -1,73 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AdminService } from '../../../Service/Staff/admin.service';
+import { WorkouplanService } from '../../../Service/Admin/Workoutplan/workouplan.service';
 
 @Component({
   selector: 'app-monitoring',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './monitoring.component.html',
   styleUrl: './monitoring.component.css'
 })
-export class MonitoringComponent {
-  members = [
-    {
-      name: 'John Doe',
-      email: 'john@example.com',
-      workoutPlans: [
-        { name: 'Plan A', startTime: '6:00 AM', checked: false },
-        { name: 'Plan B', startTime: '7:00 AM', checked: false }, 
-          { name: 'Plan C', startTime: '8:00 AM', checked: false },
-        { name: 'Plan D', startTime: '9:00 AM', checked: false }
-      ]
-    },
-    {
-      name: 'Jane Smith',
-      email: 'jane@example.com',
-      workoutPlans: [
-        { name: 'Plan C', startTime: '8:00 AM', checked: false },
-        { name: 'Plan D', startTime: '9:00 AM', checked: false }
-      ]
-    },
-    {
-      name: 'Jane Smith',
-      email: 'jane@example.com',
-      workoutPlans: [
-        { name: 'Plan C', startTime: '8:00 AM', checked: false },
-        { name: 'Plan D', startTime: '9:00 AM', checked: false }
-      ]
-    },
-    {
-      name: 'Jane Smith',
-      email: 'jane@example.com',
-      workoutPlans: [
-        { name: 'Plan C', startTime: '8:00 AM', checked: false },
-        { name: 'Plan D', startTime: '9:00 AM', checked: false }
-      ]
-    },
-    {
-      name: 'Jane Smith',
-      email: 'jane@example.com',
-      workoutPlans: [
-        { name: 'Plan C', startTime: '8:00 AM', checked: false },
-        { name: 'Plan D', startTime: '9:00 AM', checked: false }
-      ]
-    },
-    {
-      name: 'Jane Smith',
-      email: 'jane@example.com',
-      workoutPlans: [
-        { name: 'Plan C', startTime: '8:00 AM', checked: false },
-        { name: 'Plan D', startTime: '9:00 AM', checked: false }
-      ]
-    },
-    
+export class MonitoringComponent implements OnInit {
+  members: any[] = [
+
+
     // Add more members as needed
   ];
 
   selectedMember: any = null;
 
-  constructor() {}
+  constructor(private AdminService: AdminService, private WorkOutplanService: WorkouplanService) {
+
+  }
+  BranchId: any;
+  ngOnInit(): void {
+    this.BranchId = localStorage.getItem('BranchId');
+    this.loadMembers(0, 0, this.BranchId)
+  }
+  loadMembers(pageIndex: number = 0, pageSize: number = 0, branchId: number = this.BranchId): void {
+
+    this.AdminService.getAllMembers(pageIndex, pageSize, true, branchId).subscribe(response => {
+      this.members = response.data;
+      console.log(pageIndex, pageSize)
+      console.log('Loaded members:', this.members);
+
+    });
+  }
 
   openModal(member: any): void {
     this.selectedMember = member;
@@ -75,5 +44,25 @@ export class MonitoringComponent {
   closeModal() {
     this.selectedMember = null;
 
+  }
+  startTime: any;
+  endTime: any;
+  Day: any;
+
+
+  UpdateTime(workoutplan: number) {
+    console.log(this.startTime)
+    console.log(this.endTime)
+    
+    this.WorkOutplanService.updatePlanTime(workoutplan, this.startTime, this.endTime, this.Day).subscribe({
+      next: () => {
+        alert("Update Successful");
+        window.location.reload()
+      },
+      error: (err) => {
+        console.error("Update failed", err);
+        alert("There was an error updating the time. Please try again.");
+      }
+    });
   }
 }
